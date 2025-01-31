72c425193b83d4b9f8cfe27e28ec97beb86c58e0
권정훈 <righthun.kwon@gmail.com>
2025-01-31 23:29:44 +0900

    chore: vite-plugin-svgr 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import path from 'node:path';
 import electron from 'vite-plugin-electron/simple';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    svgr(),
     electron({
       main: {
         entry: 'electron/main.ts',
