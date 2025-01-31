9392b5ddce2be6fec146eebb9d08e4b70d972774
권정훈 <righthun.kwon@gmail.com>
2025-01-31 18:45:35 +0900

    design: tailwind 전역 폰트 설정

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        // 색상 정의
+      },
+      height: {
+        // 높이 정의 ,
+      },
+      width: {
+        // 너비 정의
+      },
+    },
+    fontFamily: {
+      Pretendard: ['Pretendard'],
+    },
+  },
+};
