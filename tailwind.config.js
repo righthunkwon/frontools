ed2f33d71244cee1af1ad7359155644da9b2a33b
권정훈 <righthun.kwon@gmail.com>
2025-02-02 21:00:08 +0900

    desing: 커스텀 컬러 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,11 @@ export default {
         ourBgGray: '#262626',
         ourBgBlack: '#212121',
         ourBgLightGray: '#3b3b3b',
+        ourBgLighterGray: '#747474',
         ourBgRed: '#eb2113',
         ourBgOrange: '#ff6c37',
         ourBgDarkOrange: '#e05320',
+        ourBgWhite: '#d5d5d5',
         // Ln: line (text, icon, etc)
         ourLnGray: '#a6a6a6',
         ourLnLightGray: '#373737',
